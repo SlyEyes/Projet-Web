ec0faa9351b3e3937f4f85afd8f639147c7d61d1
Julien W <cefadrom1@gmail.com>
2023-03-13 09:08:23 +0100

    feat: router setup

--- www/index.php
+++ www/index.php
@@ -3,6 +3,12 @@
 // Include the autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Import the classes
+use Klein\Klein;
+
+// Set error reporting
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 // Load the environment variables
 if (file_exists(__DIR__ . '../.env')) {
     $dotenv = new josegonzalez\Dotenv\Loader(__DIR__ . '../.env');
@@ -14,3 +20,18 @@ if (file_exists(__DIR__ . '../.env')) {
 if (!getenv('MYSQL_HOST') || !getenv('MYSQL_DATABASE') || !getenv('MYSQL_USER') || !getenv('MYSQL_PASSWORD')) {
     throw new Exception('Missing configuration variables');
 }
+
+// Configure the router
+$klein = new Klein();
+
+$klein->onHttpError(function ($code, $router) {
+    switch ($code) {
+        case 404:
+            $router->response()->body('Page not found');
+            break;
+        default:
+            $router->response()->body('An error has occurred');
+    }
+});
+
+$klein->dispatch();
