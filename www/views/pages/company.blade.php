45e6220773814acdfdd6d98ed525f8e6419e7063
Okamizz <leo.rouas@viacesi.fr>
2023-03-17 16:27:11 +0100

    Company page : - scss - Controlleur - routeur - view

---
+++ www/views/pages/company.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+
+@section('head')
+    @pagestyle('company')
+@endsection
+
+
+@section('content')
+    <main>
+        <section>
+            <div>
+        <img src="" alt=" "><h1 class="test" >Crédit Agricole</h1>
+            <div>Entreprise<div>
+            <article>
+                Note : 4.5/5
+</article>
+<h2>Stage Disponible</h2>
+</div>
+<article> 
+    <div class="box">
+<div class="a">
+Dévellopeur Web 
+chez Crédit Agricole
+<br>
+Strasbourg 4 mois
+</div>
+<div class="a">
+Dévellopeur Web 
+chez Crédit Agricole
+<br>
+Nancy 4 mois
+</div>
+</div>
+
+</article>
+
+        </section>
+    </main>
+@endsection
