1d32105108e318082ffb10e487b7edaafa5a30db
Julien W <cefadrom1@gmail.com>
2023-03-18 15:42:00 +0100

    feat: service to check the if the user has the required role

---
+++ www/src/services/AuthorizationService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Linkedout\App\services;
+
+use Linkedout\App\entities;
+use Linkedout\App\enums\RoleEnum;
+
+/**
+ * The authorization service is responsible for handling all the authorization related checks
+ */
+class AuthorizationService
+{
+    private ?entities\PersonEntity $personEntity;
+
+    function __construct(?entities\PersonEntity $personEntity)
+    {
+        $this->personEntity = $personEntity;
+    }
+
+    /**
+     * Check if the current user has one of the given roles
+     * @param RoleEnum $role1 The first role to check
+     * @param RoleEnum|null $role2 The second role to check, optional
+     * @param RoleEnum|null $role3 The third role to check, optional
+     * @return bool True if the user has one of the given roles, false otherwise
+     */
+    function hasAuthorization(RoleEnum $role1, ?RoleEnum $role2 = null, ?RoleEnum $role3 = null): bool
+    {
+        if ($this->personEntity === null)
+            return false;
+
+        $roles = [$role1, $role2, $role3];
+        $roles = array_filter($roles, fn($role) => $role !== null);
+        $roles = array_map(fn($role) => $role->value, $roles);
+        return in_array($this->personEntity->role->value, $roles);
+    }
+}
