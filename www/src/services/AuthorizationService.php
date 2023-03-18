f066739f138007aad9db9f92c215303f73ee7568
Julien W <cefadrom1@gmail.com>
2023-03-18 15:46:07 +0100

    feat: all in one redirect function

--- www/src/services/AuthorizationService.php
+++ www/src/services/AuthorizationService.php
@@ -34,4 +34,21 @@ class AuthorizationService
         $roles = array_map(fn($role) => $role->value, $roles);
         return in_array($this->personEntity->role->value, $roles);
     }
+
+    /**
+     * Redirect the user to the given path if he does not have one of the given roles
+     * @param string $path The path to redirect to
+     * @param RoleEnum $role1 The first role to check
+     * @param RoleEnum|null $role2 The second role to check, optional
+     * @param RoleEnum|null $role3 The third role to check, optional
+     * @return void
+     */
+    function redirectIfNotAuthorized(string $path, RoleEnum $role1, ?RoleEnum $role2 = null, ?RoleEnum $role3 = null): void
+    {
+        if ($this->hasAuthorization($role1, $role2, $role3))
+            return;
+
+        header("Location: {$path}");
+        exit;
+    }
 }
