3de6f416989c6f955c3d796108aa89dd8b7dfedb
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-03-21 10:50:08 +0100

    refactor: change variable $internships to $internship

--- www/src/controllers/CompanyController.php
+++ www/src/controllers/CompanyController.php
@@ -10,7 +10,7 @@ class CompanyController extends BaseController
 
     /**
      * The setter for the route parameters
-     * @param string|null $collection The ID. If null, the user will be redirected to the default collection
+     * @param string|null $id The ID. If null, the user will be redirected to the default collection
      * @return void
      */
     public function setRouteParams(?string $id): void
@@ -20,24 +20,19 @@ class CompanyController extends BaseController
 
     public function render(): string
     {
-
-
         $personModel = new models\PersonModel($this->database);
         $person = $personModel->getPersonFromJwt();
-
-
 
         $companyModel = new models\CompanyModel($this->database);
         $company = $companyModel->getCompanyById($this->id);
 
-        $internshipsmodel = new models\InternshipModel($this->database);
-        $internships = $internshipsmodel->getInternshipsByCompanyId($this->id);
-
+        $internshipModel = new models\InternshipModel($this->database);
+        $internship = $internshipModel->getInternshipByCompanyId($this->id);
 
         return $this->blade->make('pages.company', [
             'person' => $person,
             'company' => $company,
-            'internships' => $internships,
+            'internship' => $internship,
         ]); 
     }
 }
--- www/views/pages/company.blade.php
+++ www/views/pages/company.blade.php
@@ -10,25 +10,30 @@
     <main>
         <section>
             <img src="/public/icons/close-purple.svg" class="croix" alt=" " href="/">
+
             <div>
         {{--<img src="{{ $company->logo }}" alt=" "><h1 class="test" >{{ $company->name }}</h1>--}}
-            <div>Entreprise<div>
+            </div>
+
+            <h2>Entreprise</h2>
+
             <article>
                 Note : 4.5/5
-</article>
-<h2>Stage Disponible</h2>
-</div>
-<article> 
-    @foreach ($internships as $internship)
-        <div class="box">
-            <div class="a">
-                {{ $internship->title }}
-                <br>
-                {{ $internship->city }} {{ $internship->endDate }} - {{ $internship->beginDate }}
-            </div>
-        </div>
-    @endforeach
-</article>
+            </article>
+
+            <h2>Stage Disponible</h2>
+
+            <article>
+                @foreach ($internship as $i)
+                    <div class="box">
+                        <div class="a">
+                            {{ $i->title }}
+                            <br>
+                            {{ $i->city }} {{ $i->endDate }} - {{ $i->beginDate }}
+                        </div>
+                    </div>
+                @endforeach
+            </article>
 
         </section>
     </main>
