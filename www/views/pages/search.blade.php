6e0286aa1714f84c808e504a7c40e228fd83bd42
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-03-21 17:00:15 +0100

    style: filter zone with search and results list on the side

--- www/views/pages/search.blade.php
+++ www/views/pages/search.blade.php
@@ -8,34 +8,37 @@
 
 @section('content')
     <main>
-        <form action="/search" method="GET" id="search-form" title="Barre de recherche">
-            <label for="search-form-input">Barre de recherche</label>
-            <input type="text"
-                   id="search-form-input"
-                   class="input-field"
-                   name="q"
-                   placeholder="Tapez le nom d'un stage ou d'une entreprise ..."
-                   required>
-
-            <button type="submit" id="search-form-button" class="btn-primary">Rechercher</button>
-        </form>
-
         <section>
             <div id="filter-zone">
+                <h3>Filtres</h3>
 
             </div>
 
-            <ul id="results-list">
-                @foreach ($results as $result)
-                    <li class="result">
-                        <img src="{{ $result->logo }}" alt="Logo de {{ $result->name }}">
-                        <div>
-                            <h3>{{ $result->name }}</h3>
-                            <p>Secteur {{ $result->sector }}</p>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+            <div id="search-zone">
+                <form action="/search" method="GET" id="search-form" title="Barre de recherche">
+                    <label for="search-form-input">Barre de recherche</label>
+                    <input type="text"
+                           id="search-form-input"
+                           class="input-field"
+                           name="q"
+                           placeholder="Tapez le nom d'un stage ou d'une entreprise ..."
+                           required>
+
+                    <button type="submit" id="search-form-button" class="btn-primary">Rechercher</button>
+                </form>
+
+                <ul id="results-list">
+                    @foreach ($results as $result)
+                        <li class="result">
+                            <img src="{{ $result->logo }}" alt="Logo de {{ $result->name }}">
+                            <div>
+                                <h3>{{ $result->name }}</h3>
+                                <p>Secteur {{ $result->sector }}</p>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </section>
     </main>
 @endsection
