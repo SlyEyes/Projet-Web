230a74f7f85ab3847bfb2f6ca417af78dd840525
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-03-22 09:52:06 +0100

    feat: functional get request with key word and filters

--- www/resources/pages/search.js
+++ www/resources/pages/search.js
@@ -1,10 +1,3 @@
-function filterInput(radio, value) {
-    document.querySelector(radio).addEventListener('change', function() {
-        if (this.checked) {
-            document.querySelector('#filter-input').value = value;
-        }
-    });
-}
-
-filterInput('#research-type-0', 'internships');
-filterInput('#research-type-1', 'companies');
+document.querySelector('#search-bar-input').addEventListener('input', function(event) {
+    document.querySelector('#filter-search-input').value = event.target.value;
+});
--- www/views/pages/search.blade.php
+++ www/views/pages/search.blade.php
@@ -3,6 +3,7 @@
 
 @section('head')
     @pagestyle('search')
+    @pagescript('search')
 @endsection
 
 
@@ -12,36 +13,37 @@
             <div id="filter-zone">
                 <h3>Filtres</h3>
 
-                <form action="/search" method="GET" id="filters" title="Filtres">
+                <form action="/search" method="GET" id="filters" title="Barre de recherches & Filtres">
+                    <label for="filter-search-input"></label>
+                    <input type="text"
+                           id="filter-search-input"
+                           name="q"
+                           value=""
+                    >
+
                     <div class="filters-research-type">
-                        <input id="research-type-0" name="research-type" type="radio" value="internships">
-                        <label for="research-type-0">Stages</label>
+                        <input id="research-target-0" name="target" type="radio" value="internships">
+                        <label for="research-target-0">Stages</label>
                     </div>
 
                     <div class="filters-research-type">
-                        <input id="research-type-1" name="research-type" type="radio" value="companies">
-                        <label for="research-type-1">Entreprises</label>
+                        <input id="research-target-1" name="target" type="radio" value="companies">
+                        <label for="research-target-1">Entreprises</label>
                     </div>
                 </form>
             </div>
 
             <div id="search-zone">
-                <form action="/search" method="GET" id="search-bar" title="Barre de recherche">
+                <form id="search-bar" title="Barre de recherche">
                     <label for="search-bar-input"></label>
                     <input type="text"
                            id="search-bar-input"
                            class="input-field"
                            name="q"
                            placeholder="Tapez le nom d'un stage ou d'une entreprise ..."
-                           required>
-
-                    <label for="filter-input"></label>
-                    <input type="text"
-                           id="filter-input"
-                           name="f"
-                           value=""
+                           required
                     >
-                    <button type="submit" id="search-bar-button" class="btn-primary">Rechercher</button>
+                    <button type="submit" id="search-bar-button" class="btn-primary" form="filters">Rechercher</button>
                 </form>
 
                 <ul id="results-list">
