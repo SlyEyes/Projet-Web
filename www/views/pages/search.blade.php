c296c7d820d4740d1e4f5d5192f181c7b0369b67
Raphaël Denni <65921801+raphaeldenni@users.noreply.github.com>
2023-03-23 11:33:26 +0100

    feat: clickable result cards

--- www/views/pages/search.blade.php
+++ www/views/pages/search.blade.php
@@ -58,26 +58,28 @@
                         @if ($target == 'internships')
                             @foreach ($results as $result)
                                 <li class="result">
-                                    <div>
+                                    <a href="/internship/{{ $result->id }}" id="internship">
                                         <h3>{{ $result->title }}</h3>
-                                        <p>Posté par: {{ $results->companyName }}</p>
-                                        <p>Le: {{ $results->offerDate }}</p>
-                                        <p>Durée: du {{ $results->beginDate }} au {{ $results->endDate }}</p>
+                                        <p>Posté par: {{ $result->companyName }}</p>
+                                        <p>Le: {{ $result->offerDate }}</p>
+                                        <p>Durée: du {{ $result->beginDate }} au {{ $result->endDate }}</p>
                                         <p>Skills: {{ $result->skills }}</p>
-                                        <p>Nombre de places: {{ $results->numberPlaces }}</p>
-                                    </div>
+                                        <p>Nombre de places: {{ $result->numberPlaces }}</p>
+                                    </a>
                                 </li>
                             @endforeach
 
                         @elseif ($target == 'companies')
                             @foreach ($results as $result)
                                 <li class="result">
-                                    <img src="{{ $result->logo }}" alt="Logo de {{ $result->name }}">
+                                    <a href="/company/{{ $result->id }}" id="company">
+                                        <img src="{{ $result->logo }}" alt="Logo de {{ $result->name }}">
 
-                                    <div>
-                                        <h3>{{ $result->name }}</h3>
-                                        <p>Secteur: {{ $result->sector }}</p>
-                                    </div>
+                                        <div>
+                                            <h3>{{ $result->name }}</h3>
+                                            <p>Secteur: {{ $result->sector }}</p>
+                                        </div>
+                                    </a>
                                 </li>
                             @endforeach
                         @endif
