7bfb47a7756e0268bd4d6904298f1c7dd44cf342
Julien W <cefadrom1@gmail.com>
2023-03-24 18:30:20 +0100

    fix: formatting and lower-case email

--- www/views/pages/about.blade.php
+++ www/views/pages/about.blade.php
@@ -10,14 +10,26 @@
     <main class="main">
         <section class="section section-md">
             <h1>À propos</h1>
-            <h3>Qui sommes-nous ?</h3>
-            <p>Nous sommes un groupe d’indépendants qui a été chargé de créer un site Web pour votre école. Nous avons donc créé LinkedOut.</p>
-            <h3>Qu’est-ce que LinkedOut ?</h3>
-            <p>LinkedOut est un site web qui a pour but de vous aider à trouver un stage dans une entreprise en répertoriant des stages proposés par les entreprises à l'école.</p>
-            <h3>Vous avez un problème ?</h3>
-            <p> N'hésitez pas à contacter vos tuteurs ou à nous envoyer un mail à l'adresse suivante : <a href="mailto:contact@Linked-Out.fr "> contact@Linked-Out.fr </a></p>
 
-            <p><a href="" >Mentions Légales </a> </p>
+            <h3>Qui sommes-nous ?</h3>
+            <p>
+                Nous sommes un groupe d’indépendants qui a été chargé de créer un site Web pour votre école. Nous avons
+                donc créé LinkedOut.
+            </p>
+
+            <h3>Qu’est-ce que LinkedOut ?</h3>
+            <p>
+                LinkedOut est un site web qui a pour but de vous aider à trouver un stage dans une entreprise en
+                répertoriant des stages proposés par les entreprises à l'école.
+            </p>
+
+            <h3>Vous avez un problème ?</h3>
+            <p>
+                N'hésitez pas à contacter vos tuteurs ou à nous envoyer un mail à l'adresse suivante :
+                <a href="mailto:contact@linked-out.fr "> contact@linked-out.fr </a>
+            </p>
+
+            <p><a href="/legal-notice">Mentions Légales </a></p>
         </section>
     </main>
 @endsection
