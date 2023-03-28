50f05ab7ba5d2f36b56945d0e1306859cdce81f6
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-03-28 11:11:58 +0200

    feat: precise that it searches an internship

--- www/views/pages/index.blade.php
+++ www/views/pages/index.blade.php
@@ -11,7 +11,7 @@
         <section>
             <h1>Le <span>meilleur</span> moyen de trouver son <span>stage</span> à CESI</h1>
             <form action="/search"  method="GET">
-                <input type="text" name="q" placeholder="Entrez votre recherche" required>
+                <input type="text" name="q" placeholder="Entrez votre recherche de stage" required>
                 <button type="submit" class="btn-primary">Rechercher</button>
             </form>
         </section>
