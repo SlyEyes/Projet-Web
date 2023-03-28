52321c46f9465068853a2e2d2990f1d928186135
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-03-28 14:57:23 +0200

    feat: display the initial star count

--- www/views/components/grade/select.blade.php
+++ www/views/components/grade/select.blade.php
@@ -2,6 +2,7 @@
     @for($i = 0; $i < 5; $i++)
         @include('components.grade.star', [
             'id' => $i + 1,
+            'full' => $i + 1 <= $value
         ])
     @endfor
     <input type="hidden" name="{{ $name ?? null }}"
